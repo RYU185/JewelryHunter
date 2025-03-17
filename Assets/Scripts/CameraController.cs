55c67ef28126346ede2ea61955d9b13eba44ea1d
RYU185 <fence365@naver.com>
2025-03-17 15:26:55 +0900

    123

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,6 +6,14 @@ public class CameraController : MonoBehaviour
 {
     GameObject player;
 
+    public float leftLimit = 0.0f;
+    public float rightLimit = 0.0f;
+    public float bottomLimit = 0.0f;
+    public float topLimit = 0.0f;
+
+    public GameObject subScreen;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +28,34 @@ public class CameraController : MonoBehaviour
         {
             float x = player.transform.position.x;
             float y = player.transform.position.y;
-            float z = player.transform.position.z;
+            float z = this.transform.position.z;
+
+            if (x < leftLimit)
+            {
+                x = leftLimit;
+            }else if (x > rightLimit)
+            {
+                x = rightLimit;
+            }
+
+            if(y < bottomLimit)
+            {
+                y = bottomLimit;
+            } else if (y > topLimit)
+            {
+                y = topLimit;
+            }
 
             this.transform.position = new Vector3(x, y, z);
+
+            if (subScreen != null)
+            {
+                subScreen.transform.position = new Vector3(
+                    x * 0.5f,
+                    subScreen.transform.position.y,
+                    subScreen.transform.position.z
+                    );
+            }
         }
     }
 }
